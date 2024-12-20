5e64e7ffb196256c1a06c5173e2f7fa1dfabcaf1
andreselc <aelopez.20@est.ucab.edu.ve>
2024-12-19 23:20:34 -0400

    Entity interface updated with more methods

--- app/common/domain/entity.py
+++ app/common/domain/entity.py
@@ -4,3 +4,12 @@ class Entity(ABC):
     @abstractmethod
     def get(self):
         pass
+
+    @classmethod
+    @abstractmethod
+    def create(cls, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def update(self, *args, **kwargs):
+        pass
