f0e6847a97837773a3107da96b65b0cf18009a0e
andreselc <aelopez.20@est.ucab.edu.ve>
2024-12-22 21:48:22 -0400

    service to calculate prince was changed to round results

--- app/products/domain/services/calculate_price.py
+++ app/products/domain/services/calculate_price.py
@@ -4,4 +4,4 @@ def calculate_price(cost: float, margin: float) -> float:
         raise ValueError("Cost must be non-negative and margin must be between 0 and 100.")
 
     price = cost / (1 - margin / 100)
-    return price
+    return round(price, 2)
