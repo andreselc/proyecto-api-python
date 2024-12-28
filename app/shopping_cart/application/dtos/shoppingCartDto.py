43d14485b12c9a66a8934f4d23bf04b6032957af
sponcevas <129618132+ZamudiaJr@users.noreply.github.com>
2024-12-28 17:36:40 -0400

    all mappers ready

---
+++ app/shopping_cart/application/dtos/shoppingCartDto.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class ShoppinCartDto(BaseModel):
+    user_name: str
+    shoppin_cart_id: UUID
+    quantity: int
+    product_code: str
+    product_name: str
+    product_price: float
+    product_status: str
---
+++ app/shopping_cart/infrastructure/mappers/domain_to_dto.py
@@ -0,0 +1,13 @@
+from app.shopping_cart.domain.aggregate.aggregate_shoppinCart import ShoppinCartAggregate
+from app.shopping_cart.application.dtos.shoppingCartDto import ShoppinCartDto
+
+def domain_to_dto(shoppin_cart_aggregate: ShoppinCartAggregate) -> ShoppinCartDto:
+    shoppin_cart, product, user = shoppin_cart_aggregate.get()
+    return ShoppinCartDto(
+        user_name=user.name.get(),
+        shoppin_cart_id=shoppin_cart.id.get(),
+        quantity=shoppin_cart.quantity.get(),
+        product_code=product.code.get(),
+        product_price=product.price.get(),
+        product_status=product.status.value
+    )
