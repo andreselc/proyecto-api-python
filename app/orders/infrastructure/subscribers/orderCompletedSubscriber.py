7108de3aa980041d9f2db671b2720e41ba0978b0
Alex Altuve <altuvealex6@gmail.com>
2025-01-07 11:10:45 -0400

    Delete orderCompletedSubscriber.py

--- app/orders/infrastructure/subscribers/orderCompletedSubscriber.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-
-class OrderCompletedSubscriber:
-    async def handle_event(self, event):
-        await asyncio.sleep(1)  # Simula el procesamiento del evento
-        print(f"Orden completada: {event}")
